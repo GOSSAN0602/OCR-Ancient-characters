f66cc91324df6a4f1b7a929ffecc29c774eed119
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-01-29 08:14:39 +0000

    1st commit

---
+++ libs/model.py
@@ -0,0 +1,141 @@
+import sys
+sys.path.append('./')
+from libs.lazy_utils import *
+from torch import nn
+import torch.nn.functional as F
+import pretrainedmodels
+from torch.nn import Sequential
+
+
+class LinearBlock(nn.Module):
+
+    def __init__(self, in_features, out_features, bias=True,
+                 use_bn=True, activation=F.relu, dropout_ratio=-1, residual=False,):
+        super(LinearBlock, self).__init__()
+        if in_features is None:
+            self.linear = LazyLinear(in_features, out_features, bias=bias)
+        else:
+            self.linear = nn.Linear(in_features, out_features, bias=bias)
+        if use_bn:
+            self.bn = nn.BatchNorm1d(out_features)
+        if dropout_ratio > 0.:
+            self.dropout = nn.Dropout(p=dropout_ratio)
+        else:
+            self.dropout = None
+        self.activation = activation
+        self.use_bn = use_bn
+        self.dropout_ratio = dropout_ratio
+        self.residual = residual
+
+    def __call__(self, x):
+        h = self.linear(x)
+        if self.use_bn:
+            h = self.bn(h)
+        if self.activation is not None:
+            h = self.activation(h)
+        if self.residual:
+            h = residual_add(h, x)
+        if self.dropout_ratio > 0:
+            h = self.dropout(h)
+        return h
+
+class PretrainedCNN(nn.Module):
+    def __init__(self, model_name='se_resnext101_32x4d',
+                 in_channels=1, out_dim=10, use_bn=True,
+                 pretrained='imagenet'):
+        super(PretrainedCNN, self).__init__()
+        self.conv0 = nn.Conv2d(
+            in_channels, 3, kernel_size=3, stride=1, padding=1, bias=True)
+        self.base_model = pretrainedmodels.__dict__[model_name](pretrained=pretrained)
+        activation = F.leaky_relu
+        self.do_pooling = True
+        if self.do_pooling:
+            inch = self.base_model.last_linear.in_features
+        else:
+            inch = None
+        hdim = 512
+        lin1 = LinearBlock(inch, hdim, use_bn=use_bn, activation=activation, residual=False)
+        lin2 = LinearBlock(hdim, out_dim, use_bn=use_bn, activation=None, residual=False)
+        self.lin_layers = Sequential(lin1, lin2)
+
+    def forward(self, x):
+        h = self.conv0(x)
+        h = self.base_model.features(h)
+
+        if self.do_pooling:
+            h = torch.sum(h, dim=(-1, -2))
+        else:
+            # [128, 2048, 4, 4] when input is (128, 128)
+            bs, ch, height, width = h.shape
+            h = h.view(bs, ch*height*width)
+        for layer in self.lin_layers:
+            h = layer(h)
+        return h
+
+def accuracy(y, t):
+    pred_label = torch.argmax(y, dim=1)
+    count = pred_label.shape[0]
+    correct = (pred_label == t).sum().type(torch.float32)
+    acc = correct / count
+    return acc
+
+
+class BengaliClassifier(nn.Module):
+    def __init__(self, predictor, n_grapheme=168, n_vowel=11, n_consonant=7):
+        super(BengaliClassifier, self).__init__()
+        self.n_grapheme = n_grapheme
+        self.n_vowel = n_vowel
+        self.n_consonant = n_consonant
+        self.n_total_class = self.n_grapheme + self.n_vowel + self.n_consonant
+        self.predictor = predictor
+
+        self.metrics_keys = [
+            'loss', 'loss_grapheme', 'loss_vowel', 'loss_consonant',
+            'acc_grapheme', 'acc_vowel', 'acc_consonant']
+
+    def forward(self, x, y=None):
+        pred = self.predictor(x)
+        if isinstance(pred, tuple):
+            assert len(pred) == 3
+            preds = pred
+        else:
+            assert pred.shape[1] == self.n_total_class
+            preds = torch.split(pred, [self.n_grapheme, self.n_vowel, self.n_consonant], dim=1)
+        loss_grapheme = F.cross_entropy(preds[0], y[:, 0])
+        loss_vowel = F.cross_entropy(preds[1], y[:, 1])
+        loss_consonant = F.cross_entropy(preds[2], y[:, 2])
+        loss = loss_grapheme + loss_vowel + loss_consonant
+        metrics = {
+            'loss': loss.item(),
+            'loss_grapheme': loss_grapheme.item(),
+            'loss_vowel': loss_vowel.item(),
+            'loss_consonant': loss_consonant.item(),
+            'acc_grapheme': accuracy(preds[0], y[:, 0]),
+            'acc_vowel': accuracy(preds[1], y[:, 1]),
+            'acc_consonant': accuracy(preds[2], y[:, 2]),
+        }
+        return loss, metrics, pred
+
+    def calc(self, data_loader):
+        device: torch.device = next(self.parameters()).device
+        self.eval()
+        output_list = []
+        with torch.no_grad():
+            for batch in tqdm(data_loader):
+                # Caution: support general preprocessing.
+                # If `data` is not `Data` instance, `to` method is not supported!
+                batch = batch.to(device)
+                pred = self.predictor(batch)
+                output_list.append(pred)
+        output = torch.cat(output_list, dim=0)
+        preds = torch.split(output, [self.n_grapheme, self.n_vowel, self.n_consonant], dim=1)
+        return preds
+
+    def predict_proba(self, data_loader):
+        preds = self.calc(data_loader)
+        return [F.softmax(p, dim=1) for p in preds]
+
+    def predict(self, data_loader):
+        preds = self.calc(data_loader)
+        pred_labels = [torch.argmax(p, dim=1) for p in preds]
+        return pred_labels
